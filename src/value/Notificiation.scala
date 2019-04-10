f305e1c7d8244fa42897018ba9c89db29b1cfc27
Daniel Waters <danwater1@gmail.com>
2019-04-09 20:14:17 -0700

    Finishes test and implementation of Notification

--- src/value/Notificiation.scala
+++ src/value/Notificiation.scala
@@ -1,5 +1,17 @@
 package value
 
-class Notificiation extends Value {
+case class Notification(private val noteType: String) extends Value {
+    override def toString = f"${noteType}"
+}
+
+object Notification {
   
+  val OK = "ok"
+  val DONE = "done"
+  val UNSPECIFIED = "unspecified"
+  
+  def apply(noteType: String) = noteType match {
+    case OK | DONE | UNSPECIFIED => new Notification(noteType)
+    case _ => "Type needs to be either OK, DONE, or UNSPECIFIED" 
+  }
 }
