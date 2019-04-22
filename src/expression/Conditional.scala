761cb9dca21a9bec16e31aa1509483c8292a2b35
Daniel Waters <danwater1@gmail.com>
2019-04-22 14:06:37 -0700

    Fixes bug in default value in conditional

--- src/expression/Conditional.scala
+++ src/expression/Conditional.scala
@@ -3,11 +3,12 @@ package expression
 import context._
 import value._
 
-case class Conditional(val condition: Expression, consequent: Expression, alternative: Expression = Boole(true)) extends SpecialForm {
+case class Conditional(val condition: Expression, consequent: Expression, alternative: Expression = null) extends SpecialForm {
   def execute(env: Environment): Value = {
     if (condition.execute(env).equals(Boole(true))) {
       consequent.execute(env)
     } else if(condition.execute(env).equals(Boole(false)) && alternative != null) {
+      println(alternative)
       alternative.execute(env)
     } else {
       Notification.UNSPECIFIED
