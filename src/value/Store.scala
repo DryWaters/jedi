adf143c475c8cfb3644afbac8f0b1ad3d4eb2f22
Daniel Waters <danwater1@gmail.com>
2019-05-01 19:20:37 -0700

    Adds type checking for filter/map in store

--- src/value/Store.scala
+++ src/value/Store.scala
@@ -26,8 +26,22 @@ class Store(private var elems: ArrayBuffer[Value] = ArrayBuffer[Value]()) extend
   override def toString = "{" + elems.mkString(" ") + "}"
 
   // returns store containing the elements of this transformed by trans
-  def map(trans: Closure): Store = { new Store(elems.map(elem => trans(elem :: Nil))) }
+  def map(trans: Closure): Store = {
+    if (elems.length == 0) return this
+
+    new Store(elems.map(elem => trans(elem :: Nil)))
+  }
 
   // returns store containing the elements of this that passed test
-  def filter(test: Closure): Store = { new Store(elems.filter(elem => test(elem :: Nil).asInstanceOf[Boole].value)) }
+  def filter(test: Closure): Store = {
+
+    // if trying to filter an empty Store just return Store
+    if (elems.length == 0) return this
+
+    // if trying to filter with a Closure that does not return a Boole, throw error
+    if (!test(elems(0) :: Nil).isInstanceOf[Boole])
+      throw new TypeException("filter expects a closure that returns Boole")
+
+    new Store(elems.filter(elem => test(elem :: Nil).asInstanceOf[Boole].value))
+  }
 }
